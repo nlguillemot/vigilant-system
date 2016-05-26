e944172762329e456386c18fa16636dd4049f4e0
Nicolas Guillemot <nlguillemot@gmail.com>
2016-05-26 09:16:38 -0700

    adjust sizes to suggested larrabee sizes, tweak namings

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -7,21 +7,21 @@
 // Fine raster works on blocks of 2x2 pixels, since NEON can fine rasterize a block in parallel using SIMD-4.
 // Coarse raster works on 2x2 blocks of fine blocks, since 4 fine blocks are processed in parallel. Therefore, 4x4 pixels per coarse block.
 // Tile raster works on 2x2 blocks of coarse blocks, since 4 coarse blocks are processed in parallel. Therefore, 8x8 pixels per tile.
-#define FRAMEBUFFER_TILE_SIZE_IN_PIXELS 8
-#define FRAMEBUFFER_COARSE_BLOCK_SIZE_IN_PIXELS 4
-#define FRAMEBUFFER_FINE_BLOCK_SIZE_IN_PIXELS 2
+#define FRAMEBUFFER_TILE_WIDTH_IN_PIXELS 128
+#define FRAMEBUFFER_COARSE_BLOCK_WIDTH_IN_PIXELS 16
+#define FRAMEBUFFER_FINE_BLOCK_WIDTH_IN_PIXELS 4
 
 // The swizzle masks, using alternating yxyxyx bit pattern for morton-code swizzling pixels in a tile.
 // This makes the pixels morton code swizzled within every rasterization level (fine/coarse/tile)
-// The tiles themselevs are stored row major.
+// The tiles themselves are stored row major.
 // For examples of this concept, see:
 // https://software.intel.com/en-us/node/514045
 // https://msdn.microsoft.com/en-us/library/windows/desktop/dn770442%28v=vs.85%29.aspx
-#define FRAMEBUFFER_TILE_X_SWIZZLE_MASK 0b01'0101
-#define FRAMEBUFFER_TILE_Y_SWIZZLE_MASK 0b10'1010
+#define FRAMEBUFFER_TILE_X_SWIZZLE_MASK (0x55555555 & (FRAMEBUFFER_TILE_WIDTH_IN_PIXELS - 1))
+#define FRAMEBUFFER_TILE_Y_SWIZZLE_MASK (~FRAMEBUFFER_TILE_X_SWIZZLE_MASK & (FRAMEBUFFER_TILE_WIDTH_IN_PIXELS - 1))
 
 // Convenience
-#define FRAMEBUFFER_PIXELS_PER_TILE (FRAMEBUFFER_TILE_SIZE_IN_PIXELS * FRAMEBUFFER_TILE_SIZE_IN_PIXELS)
+#define FRAMEBUFFER_PIXELS_PER_TILE (FRAMEBUFFER_TILE_WIDTH_IN_PIXELS * FRAMEBUFFER_TILE_WIDTH_IN_PIXELS)
 
 typedef struct framebuffer_t
 {
@@ -30,9 +30,10 @@ typedef struct framebuffer_t
     uint32_t width_in_pixels;
     uint32_t height_in_pixels;
     
-    // number of pixels in a row of tiles (num_tiles_per_row * num_pixels_per_tile)
-    uint32_t pixels_per_tile_row;
-    // number of pixels in the whole image (num_pixels_per_tile_row * num_tile_rows)
+    // num_tiles_per_row * num_pixels_per_tile
+    uint32_t pixels_per_row_of_tiles;
+
+    // pixels_per_row_of_tiles * num_tile_rows
     uint32_t pixels_per_slice;
 } framebuffer_t;
 
@@ -51,11 +52,11 @@ framebuffer_t* new_framebuffer(uint32_t width, uint32_t height)
 
     // pad framebuffer up to size of next tile
     // that way the rasterization code doesn't have to handlep otential out of bounds access after tile binning
-    int32_t padded_width_in_pixels = (width + (FRAMEBUFFER_TILE_SIZE_IN_PIXELS - 1)) & -FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
-    int32_t padded_height_in_pixels = (height + (FRAMEBUFFER_TILE_SIZE_IN_PIXELS - 1)) & -FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
+    int32_t padded_width_in_pixels = (width + (FRAMEBUFFER_TILE_WIDTH_IN_PIXELS - 1)) & -FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
+    int32_t padded_height_in_pixels = (height + (FRAMEBUFFER_TILE_WIDTH_IN_PIXELS - 1)) & -FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
 
-    fb->pixels_per_tile_row = padded_width_in_pixels * FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
-    fb->pixels_per_slice = padded_height_in_pixels / FRAMEBUFFER_TILE_SIZE_IN_PIXELS * fb->pixels_per_tile_row;
+    fb->pixels_per_row_of_tiles = padded_width_in_pixels * FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
+    fb->pixels_per_slice = padded_height_in_pixels / FRAMEBUFFER_TILE_WIDTH_IN_PIXELS * fb->pixels_per_row_of_tiles;
 
     fb->backbuffer = (pixel_t*)malloc(fb->pixels_per_slice * sizeof(pixel_t));
     assert(fb->backbuffer);
@@ -91,14 +92,14 @@ void framebuffer_pack_row_major(framebuffer_t* fb, uint32_t x, uint32_t y, uint3
     assert(y + height <= fb->height_in_pixels);
     assert(data);
 
-    uint32_t topleft_tile_y = y / FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
-    uint32_t topleft_tile_x = x / FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
-    uint32_t bottomright_tile_y = (y + height - 1) / FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
-    uint32_t bottomright_tile_x = (x + width - 1) / FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
+    uint32_t topleft_tile_y = y / FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
+    uint32_t topleft_tile_x = x / FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
+    uint32_t bottomright_tile_y = (y + (height - 1)) / FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
+    uint32_t bottomright_tile_x = (x + (width - 1)) / FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
     
     uint32_t dst_i = 0;
 
-    uint32_t curr_tile_row_start = topleft_tile_y * fb->pixels_per_tile_row + topleft_tile_x * FRAMEBUFFER_PIXELS_PER_TILE;
+    uint32_t curr_tile_row_start = topleft_tile_y * fb->pixels_per_row_of_tiles + topleft_tile_x * FRAMEBUFFER_PIXELS_PER_TILE;
     for (uint32_t tile_y = topleft_tile_y; tile_y <= bottomright_tile_y; tile_y++)
     {
         uint32_t curr_tile_start = curr_tile_row_start;
@@ -106,15 +107,15 @@ void framebuffer_pack_row_major(framebuffer_t* fb, uint32_t x, uint32_t y, uint3
         for (uint32_t tile_x = topleft_tile_x; tile_x <= bottomright_tile_x; tile_x++)
         {
             for (uint32_t pixel_y = 0, pixel_y_bits = 0;
-                pixel_y < FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
+                pixel_y < FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
                 pixel_y++, pixel_y_bits = (pixel_y_bits - FRAMEBUFFER_TILE_Y_SWIZZLE_MASK) & FRAMEBUFFER_TILE_Y_SWIZZLE_MASK)
             {
                 for (uint32_t pixel_x = 0, pixel_x_bits = 0;
-                    pixel_x < FRAMEBUFFER_TILE_SIZE_IN_PIXELS;
+                    pixel_x < FRAMEBUFFER_TILE_WIDTH_IN_PIXELS;
                     pixel_x++, pixel_x_bits = (pixel_x_bits - FRAMEBUFFER_TILE_X_SWIZZLE_MASK) & FRAMEBUFFER_TILE_X_SWIZZLE_MASK)
                 {
-                    uint32_t src_y = tile_y * FRAMEBUFFER_TILE_SIZE_IN_PIXELS + pixel_y;
-                    uint32_t src_x = tile_x * FRAMEBUFFER_TILE_SIZE_IN_PIXELS + pixel_x;
+                    uint32_t src_y = tile_y * FRAMEBUFFER_TILE_WIDTH_IN_PIXELS + pixel_y;
+                    uint32_t src_x = tile_x * FRAMEBUFFER_TILE_WIDTH_IN_PIXELS + pixel_x;
 
                     // don't copy pixels outside src rectangle region
                     if (src_y < y || src_y >= y + height)
@@ -152,7 +153,7 @@ void framebuffer_pack_row_major(framebuffer_t* fb, uint32_t x, uint32_t y, uint3
             curr_tile_start += FRAMEBUFFER_PIXELS_PER_TILE;
         }
 
-        curr_tile_row_start += fb->pixels_per_tile_row;
+        curr_tile_row_start += fb->pixels_per_row_of_tiles;
     }
 }
 
